0ad3915fd9e24fd13d2ed82e17c91b57689c4da7
Sears Dev <dev@md.searsdev.net>
2013-02-19 11:05:39 -0600

    Removed prepending of http onto url

--- image-widget.php
+++ image-widget.php
@@ -86,12 +86,7 @@ class WidgetPress_Image_Widget extends WP_Widget {
 		    if($subtitle) : ?> <h4><?php echo $subtitle; ?></h4><?php endif;
             echo $after_title; 
         endif;
-		
-		if($url)
-		{
-			$url = "http://" . str_replace("http://", "", $url);
-		}
-				
+					
 ?>
 
         	<article class="content-body">
